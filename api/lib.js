05fe8e221f7b3b4b493275181fede3c04b640e36
Lemtix YT <lemtixyt@gmail.com>
2026-06-10 19:27:04 +0530

    Update lib.js

--- api/lib.js
+++ api/lib.js
@@ -10,7 +10,7 @@ async function callGoogleGemini(promptText, maxOutputTokens = 1200) {
   }
 
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta1/models/${GOOGLE_MODEL}:generateContent?key=${GOOGLE_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1/models/${GOOGLE_MODEL}:generateContent?key=${GOOGLE_API_KEY}`,
     {
       method: 'POST',
       headers: {
